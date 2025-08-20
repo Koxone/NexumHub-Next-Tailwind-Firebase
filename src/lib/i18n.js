f0f87ad974a8bb92f72b8130fd3e67e42519074c
Carlos de Leon <admin@koxland.net>
2025-08-19 23:26:13 -0600

    Update i18n.js

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -1,9 +1,9 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import en from '@/locales/en/translation.json';
-import es from '@/locales/es/translation.json';
-import pt from '@/locales/pt/translation.json';
+import en from '../locales/en/translation';
+import es from '../locales/es/translation.json';
+import pt from '../locales/pt/translation.json';
 
 i18n.use(initReactI18next).init({
   resources: {

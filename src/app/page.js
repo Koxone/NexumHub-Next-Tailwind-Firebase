d39e7cf25d4240193dc23a9c8aee59fb6e138b16
Carlos de Leon <admin@koxland.net>
2025-08-23 17:48:06 -0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div
       className={[
-        'mobile: grid min-h-full w-full gap-12 justify-self-center overflow-x-hidden',
+        'mobile: grid min-h-full w-full gap-12 justify-self-center',
         'sm:',
         'md:w-full md:grid-rows-[auto_auto_1fr]',
         'lg:max-w-[672px] lg:place-self-center',
